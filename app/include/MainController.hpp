98e5a9b66d7a0050e92289ecb88018c09f5a9895
knezevic.vojin <knezevic.vojin9@gmail.com>
2025-10-03 12:10:22 +0200

    Added test model, backpack

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -8,6 +8,13 @@ class MainController : public engine::core::Controller{
     void initialize() override;
 
 bool loop() override;
+    void begin_draw() override;
+    void end_draw() override;
+    void draw_backpack();
+    void draw() override;
+
+    void update_camera();
+    void update() override;
 
 public:
     std::string_view name() const override {
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -1,10 +1,15 @@
 #include "../include/MainController.hpp"
 
+#include "engine/graphics/GraphicsController.hpp"
+#include "engine/graphics/OpenGL.hpp"
 #include "engine/platform/PlatformController.hpp"
+#include "engine/resources/Mesh.hpp"
+#include "engine/resources/ResourcesController.hpp"
 #include "spdlog/spdlog.h"
 
 namespace app {
 void MainController::initialize() {
+    engine::graphics::OpenGL::enable_depth_testing();
     spdlog::info("Main controller initialized!!");
 }
 bool MainController::loop() {
@@ -14,6 +19,63 @@ bool MainController::loop() {
     }
 
     return true;
+}
+void MainController::begin_draw() {
+    engine::graphics::OpenGL::clear_buffers();
+}
+void MainController::end_draw() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    platform->swap_buffers();
+}
+void MainController::draw_backpack() {
+    // Model
+    auto resource = engine::core::Controller::get<engine::resources::ResourcesController>();
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    engine::resources::Model *backpack = resource->model("backpack");
+
+    engine::resources::Shader *shader = resource->shader("basic");
+
+    shader->use();
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+    model = glm::scale(model, glm::vec3(0.3f));
+
+    shader->set_mat4("model", model);
+
+    backpack->draw(shader);
+}
+void MainController::draw() {
+    draw_backpack();
+}
+void MainController::update_camera() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+
+    auto camera = graphics->camera();
+    float dt = platform->dt();
+    if (platform->key(engine::platform::KEY_W).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::FORWARD, dt);
+    }
+    if (platform->key(engine::platform::KEY_S).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::BACKWARD, dt);
+    }
+    if (platform->key(engine::platform::KEY_A).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::LEFT, dt);
+    }
+    if (platform->key(engine::platform::KEY_D).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::RIGHT, dt);
+    }
+    if (platform->key(engine::platform::KEY_LEFT_SHIFT).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::DOWN, dt);
+    }
+    if (platform->key(engine::platform::KEY_SPACE).is_down()) {
+        camera->move_camera(engine::graphics::Camera::Movement::UP, dt);
+    }
+}
+void MainController::update() {
+    update_camera();
 }
 
 }// namespace app
