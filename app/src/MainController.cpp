78a9ed84df8fdb20601a677063f294adf7b91a36
knezevic.vojin <knezevic.vojin9@gmail.com>
2025-10-03 11:57:23 +0200

    close app on esc

--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -1,11 +1,19 @@
 #include "../include/MainController.hpp"
 
+#include "engine/platform/PlatformController.hpp"
 #include "spdlog/spdlog.h"
 
 namespace app {
-    void MainController::initialize() {
-        spdlog::info("Main controller initialized!!");
+void MainController::initialize() {
+    spdlog::info("Main controller initialized!!");
+}
+bool MainController::loop() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    if (platform->key(engine::platform::KEY_ESCAPE).is_down()) {
+        return false;
+    }
 
+    return true;
 }
 
 }// namespace app
